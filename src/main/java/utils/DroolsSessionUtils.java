03cc8c5cef04ee115b6bc9034836d6978c1e0900
mklimasz <mk.klimaszewski@gmail.com>
2016-01-04 15:11:54 +0100

    Removing unnecessary comments

--- src/main/java/utils/DroolsSessionUtils.java
+++ src/main/java/utils/DroolsSessionUtils.java
@@ -19,33 +19,23 @@ public class DroolsSessionUtils {
     public static KieSession createKieSession(String... drlResourcesPaths) {
         KieServices ks = KieServices.Factory.get();
         KieContainer kcontainer = createKieContainer(ks, drlResourcesPaths);
-
-        // Configure and create the KieBase
         KieBaseConfiguration kbconf = ks.newKieBaseConfiguration();
         KieBase kbase = kcontainer.newKieBase(kbconf);
-
-        // Configure and create the KieSession
         KieSessionConfiguration ksconf = ks.newKieSessionConfiguration();
         return kbase.newKieSession(ksconf, null);
     }
 
     private static KieContainer createKieContainer(KieServices ks, String... drlResourcesPaths) {
-        // Create the in-memory File System and add the resources files to it
         KieFileSystem kfs = ks.newKieFileSystem();
         for (String path : drlResourcesPaths) {
             kfs.write(ResourceFactory.newClassPathResource(path));
         }
-        // Create the builder for the resources of the File System
         KieBuilder kbuilder = ks.newKieBuilder(kfs);
-        // Build the Kie Bases
         kbuilder.buildAll();
-        // Check for errors
         if (kbuilder.getResults().hasMessages(Message.Level.ERROR)) {
             throw new IllegalArgumentException(kbuilder.getResults().toString());
         }
-        // Get the Release ID (mvn style: groupId, artifactId,version)
         ReleaseId relId = kbuilder.getKieModule().getReleaseId();
-        // Create the Container, wrapping the KieModule with the given ReleaseId
         return ks.newKieContainer(relId);
     }
 }
